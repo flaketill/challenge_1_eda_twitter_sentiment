75b9acc07a769e365dd117b6bcc829ef463353e6
flaketill <armandoibarra1@gmail.com>
2023-06-14 23:54:16 -0600

    Update tweepy method OAuth1UserHandler

--- src/app.py
+++ src/app.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# -*- encoding: UTF-8 -*
+
+
+import os
+import pandas as pd
 import tweepy
 from textblob import TextBlob
+import logging, time
+from dotenv import load_dotenv # Persistent Virtualenv Environment Variables with python-dotenv
+
 
 # Step 1 - Authenticate
 consumer_key= 'CONSUMER_KEY_HERE'
@@ -8,25 +18,14 @@ consumer_secret= 'CONSUMER_SECRET_HERE'
 access_token='ACCESS_TOKEN_HERE'
 access_token_secret='ACCESS_TOKEN_SECRET_HERE'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret,
+    access_token, access_token_secret
+)
 
+
+# Step 2 - Instantiate the tweepy API
 api = tweepy.API(auth)
 
 #Step 3 - Retrieve Tweets
 public_tweets = api.search('Trump')
-
-
-
-#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
-#and label each one as either 'positive' or 'negative', depending on the sentiment 
-#You can decide the sentiment polarity threshold yourself
-
-
-for tweet in public_tweets:
-    print(tweet.text)
-    
-    #Step 4 Perform Sentiment Analysis on Tweets
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
-    print("")
